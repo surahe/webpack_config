f1d69b4050f42b552583ae9ddb02444429bfef2d
Sura <375262986@qq.com>
2018-12-16 19:08:38 +0800

    添加babel相关

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,15 +1,24 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const common = require('./webpack.common.js');
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const common = require('./webpack.common.js')
+const utils = require('./utils')
+const config = require('../config')
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map',
+  devtool: config.build.productionSourceMap ? '#source-map' : false,
   optimization: {
     splitChunks: {
-      chunks: 'all'
-    }
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    },
+    runtimeChunk: 'single'
   },
   plugins: [
     new UglifyJSPlugin({
@@ -17,6 +26,11 @@ module.exports = merge(common, {
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  ]
+    }),
+    new webpack.HashedModuleIdsPlugin()
+  ],
+  output: {
+    filename: utils.assetsPath('js/[name].[chunkhash].js'),
+    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+  }
 });
